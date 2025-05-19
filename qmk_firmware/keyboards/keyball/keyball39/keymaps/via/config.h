ca83e06310da6773fa0007f55d78de3277a4f8d1
hiback <5335514+hiback@users.noreply.github.com>
2025-05-20 08:42:16 +0900

    Change CPI for win

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -43,14 +43,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define AUTO_DARG_ON_LAYER 3
 #define AUTO_SNIPING_ON_LAYER 1
-#if system_os == 0
+#if SYSTEM_OS == 0
 #define NORMAL_CPI 1600
 #define KEYBALL_CPI_DEFAULT NORMAL_CPI
 #define SNIPING_CPI 400
 #else
-#define NORMAL_CPI 1600
+#define NORMAL_CPI 1200
 #define KEYBALL_CPI_DEFAULT NORMAL_CPI
-#define SNIPING_CPI 400
+#define SNIPING_CPI 300
 #endif
 #define QUICK_TAP_TERM_PER_KEY
 #define QUICK_TAP_TERM 200
